1cc7bd8e81292d47128edac95f458fff66a9ab96
Michael Winkler <michael.winkler@micwin.net>
2014-10-08 07:50:49 +0200

    Removed obsolete libraries, switched to MIT License, clearified documentation

--- ticino-examples/src/main/java/net/micwin/examples/example1/Receiver.java
+++
@@ -1,7 +0,0 @@
-package net.micwin.examples.example1;
-
-public class Receiver {
-	public void doSomethingWithThisEvent(IncomingXmlEvent incomingXml) {
-		System.out.println("incoming xml : " + incomingXml.getXml());
-	}
-}
--- ticino-examples/src/main/java/net/micwin/examples/example1/XmlCaster.java
+++
@@ -1,9 +0,0 @@
-package net.micwin.examples.example1;
-
-import net.micwin.ticino.Ticino;
-
-public class XmlCaster {
-	public void yep(String newXml) {
-		Ticino.dispatch(new IncomingXmlEvent(newXml));
-	}
-}
