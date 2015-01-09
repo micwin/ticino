bd70d5c386345c4693e4f311b77750d8f4591922
Michael Winkler <michael.winkler@micwin.net>
2015-01-09 07:16:03 +0100

    Skeletton of static EventScopes approach

---
+++ static/src/main/java/net/micwin/ticino/manager/EventScopes.java
@@ -0,0 +1,18 @@
+
+package net.micwin.ticino.manager ;
+
+/**
+ * A static context for multiple event scope contexts.
+ * 
+ * @author micwin
+ */
+public class EventScopes {
+
+    /**
+     * Restart all event scope contexts.
+     */
+    protected static void restart () {
+
+    }
+
+}
---
+++ static/src/test/java/net/micwin/ticino/manager/EventScopesTest.java
@@ -0,0 +1,19 @@
+
+package net.micwin.ticino.manager ;
+
+import org.junit.* ;
+
+public class EventScopesTest extends EventScopes {
+
+    @Before
+    public void before () {
+
+        EventScopes.restart () ;
+    }
+
+    @Test
+    public void testSmoke () {
+
+    }
+
+}
